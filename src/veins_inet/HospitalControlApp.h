59d198b7f6077099e71e81d533a86da93093eb92
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-06-21 14:15:25 +0700

    add new attribute for counting

--- src/veins_inet/HospitalControlApp.h
+++ src/veins_inet/HospitalControlApp.h
@@ -61,6 +61,7 @@ private:
     void exponentialSmoothing(NodeVertex *nv, double stopTime);
     void readLane(AGV *cur, std::string str);
     double lastUpdate = 0.0;
+    long count = 0;
     double getAvailablePerdestrian(std::string crossId, double _time);
     double getVeloOfPerdestrian(std::string crossId, double _time);
 };
