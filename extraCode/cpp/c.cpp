115bbfd27740522385d7a307519af9f72c2842dc
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-06-23 20:02:56 +0700

    attemp to write weight edges into file

--- extraCode/cpp/c.cpp
+++ extraCode/cpp/c.cpp
@@ -1,8 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
 int count=0;
 int N;
+vector<string> edges;
 vector<string> res;
+
+void intersection(string str){
+    int it=0;
+    string a;
+    size_t pos;
+    size_t len;
+    for(int i=0;i<str.length();i++){
+        if(str[i]==' '){
+            pos=++i;
+            len=0;
+            while(str[i]!='_'&&str[i]!=' '){
+                len++;
+                i++;
+            }
+            if(str[i]==' '){
+                i--;
+                continue;
+            }
+            a=str.substr(pos,len);
+            it++;
+        }
+    }
+    if(it>=2) {
+        if(find(res.begin(),res.end(),a)==res.end()) res.push_back(a);
+    }
+}
+
+
 void line_morphing(string str){
     int it=0;
     string num;
@@ -34,23 +65,39 @@ void line_morphing(string str){
         }
     }
     if(it>=2){
-        res.push_back(non_int+'$'+non_int+key+' '+num);
+        edges.push_back(non_int+'$'+non_int+key+' '+num);
         num='0';
-        res.push_back(non_int+key+'$'+key+' '+num);
+        edges.push_back(non_int+key+'$'+key+' '+num);
     } else {
-        res.push_back(non_int+'$'+key+' '+num);
+        edges.push_back(non_int+'$'+key+' '+num);
     }
 }
 int main(){
+    //Cau a
+    fstream file;
+    file.open("input.txt",ios::in);
+    string str;
+    cout<<"Prepare to read file...."<<endl;
+    while(getline(file,str)){
+        intersection(str);
+    }
+    cout<<res.size();
+    file.close();
+    //Assign trong so 
+    //for(int i = 0; i < res.size(); i++){
+    //    double num=3.0*(rand()%100)/100;
+        //outfile<<res[i]<<' '<<num<<'\n';
+        //cout<<res[i]<<' '<<num<<'\n';
+    //}
     //Câu c
     fstream newfile;
     newfile.open("input.txt",ios::in);
     ofstream outfile("edges.txt");
-    string str;
+    //string str;
     while(getline(newfile,str)){
         line_morphing(str);
     }
-    for(string a:res){
+    for(string a:edges){
         outfile<<a<<'\n';
     }
     newfile.close();
