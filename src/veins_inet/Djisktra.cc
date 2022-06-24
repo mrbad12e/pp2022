e4ee99d84379ae9683243de5ce309ce388cca303
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-06-25 00:07:46 +0700

    cc file for Djisktra algorithm

---
+++ src/veins_inet/Djisktra.cc
@@ -0,0 +1,186 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "Djisktra.h"
+
+Djisktra::Djisktra() {
+    // TODO Auto-generated constructor stub
+
+}
+
+Djisktra::~Djisktra() {
+    // TODO Auto-generated destructor stub
+}
+
+void Djisktra::getListVertices(std::string iVertices, std::string bVertices) {
+    std::ifstream file1(iVertices);
+    std::string line;
+
+    while (getline(file1, line)) {
+        vertices.push_back(line);
+        weightVertices.push_back(0);
+    }
+    file1.close();
+    int endOfI_Vertices = vertices.size();
+    std::ifstream file2(bVertices);
+    std::string dst = "";
+    while (getline(file2, line)) {
+        dst = "";
+        for(int k = line.length() - 1; k > 0; k--){
+            if(line[k] != '_'){
+                dst = line[k] + dst;
+            }
+            else{
+                break;
+            }
+        }
+        for(int u = 0; u < endOfI_Vertices; u++){
+            if(dst.compare(vertices[u]) == 0){
+                nextIndexOfBVertices.push_back(u);
+                nextNameOfBVertices.push_back(dst);
+                break;
+            }
+        }
+        vertices.push_back(line);
+        weightVertices.push_back(0);
+        traces.push_back("");
+    }
+    file2.close();
+}
+
+void Djisktra::getListEdges(std::string weightEdges){
+    std::ifstream file(weightEdges);
+    std::string line;
+    std::string name, weight;
+    double w;
+    while (getline(file, line)) {
+        std::stringstream ss(line);
+        getline(ss, name, ' ');
+        getline(ss, weight, ' ');
+        w =std::stod(weight);
+        edges.push_back(std::make_pair(name, w));
+    }
+    file.close();
+}
+
+void Djisktra::createAndAddEdge(vector <Quad> adjList[], int u, double weightEdge, double weightVertex, string v, int indexOfV){
+    weightVertices[u] = weightVertex;
+    adjList[u].push_back(make_tuple(weightEdge, v, indexOfV, v));
+} // createAndAddEdge
+
+void Djisktra::generateAdj(std::vector <Quad> adjList[]){
+    std::vector<int> locations;
+    std::string src = "";
+    std::string dst = "";
+    int indexOfDst = -1;
+    for(int i = 0; i < numIVertices; i++){
+        src = "$" + vertices[i] + "$";
+        for(int j = 0; j < edges.size(); j++){
+            if(src.compare(edges[j].first.substr(0, src.length())) == 0){
+                dst = "";
+                //assign dst: name of the next vertex
+                for(int k = edges[j].first.length() - 2; k > 0; k--){
+                    if(edges[j].first[k] != '$'){
+                        dst = edges[j].first[k] + dst;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                indexOfDst = -1;
+                for(int u = numIVertices; u < numVertices; u++){
+                    if(dst.compare(vertices[u]) == 0){
+                        indexOfDst = u;
+                        break;
+                    }
+                }
+                locations.push_back(j);
+                createAndAddEdge(adjList, i, edges[j].second, 0, edges[j].first, indexOfDst);
+                createAndAddEdge(adjList, indexOfDst, 0, 0,
+                        nextNameOfBVertices[indexOfDst - numIVertices], nextIndexOfBVertices[indexOfDst - numIVertices]);
+            }
+        }
+        for(int k = locations.size() - 1; k >= 0; k--){
+            edges.erase(edges.begin() + locations[k]);
+        }
+        locations.clear();
+    }
+}
+void Djisktra::DijkstrasAlgorithm(std::vector <Quad> adjList[], int source, int target){
+  priority_queue<Quad, vector<Quad>, greater<Quad> > PQ; // Set up priority queue
+  Quad info;
+  std::string trace;
+  double weight;
+  double tempW;
+  int tempIndex;
+  std::string tempTrace;
+  ShortestPath[source] = 0; // Set source distance to zero
+  std::vector <bool> visitedVertex(numVertices, false);
+
+  for (int i = 0; i < numVertices; i++)
+    if (i != source)
+      ShortestPath[i] = 100000; // Initialize everything else to +infinity
+
+  PQ.push(make_tuple(0, vertices[source], source, "")); // Source has weight 0;
+
+  while (!PQ.empty()){
+    info = PQ.top(); // Use to get minimum weight
+    PQ.pop(); // Pop before checking for cycles
+    source = get<2>(info); // get the vertex
+    if(source == target)
+      break;
+    weight = get<0>(info); // current distance
+    trace = get<3>(info);
+
+
+    if (visitedVertex.at(source)) // Check for cycle
+      continue; // Already accounted for it, move on
+
+    visitedVertex.at(source) = true; // Else, mark the vertex so that we won't have to visit it again
+
+    for (vector<Quad>::iterator it = adjList[source].begin(); it != adjList[source].end(); it++){
+      tempW = get<0>(*it);
+      tempIndex = get<2>(*it);
+
+
+      if ((weight + tempW + weightVertices[tempIndex]) < ShortestPath[tempIndex]){ // Check if we can do better
+         tempTrace = get<3>(*it);
+         ShortestPath[tempIndex] = weight + tempW + weightVertices[tempIndex]; // Update new distance
+         traces[tempIndex] = trace; //tempTrace;
+         PQ.push(make_tuple(ShortestPath[tempIndex], vertices[tempIndex], tempIndex, trace + tempTrace)); // Push vertex and weight onto Priority Queue
+      } // Update distance
+    }
+  } // While Priority Queue is not empty
+} // DijkstrasAlgorithm
+
+std::string Djisktra::getRoute(std::string trace){
+  std::string route = "";
+  std::string temp = "";
+  for(int i = 1; i < trace.length(); i++){
+    //if(temp.length() == 0){
+      if(trace[i] != '$' && trace[i] != '_'){
+        temp = temp + trace[i];
+      }
+      else{
+
+        if(temp[0] != ':' && route.find(temp + " ") == std::string::npos){
+          route = route + temp + " ";
+        }
+        temp = "";
+      }
+    //}
+  }
+  return route;
+}
