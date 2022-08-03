0f179d470da898892f371797cf35fb05c38bfd9c
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-08-03 15:39:26 +0700

    prolong the time for catching error

--- src/veins_inet/HospitalControlApp.cc
+++ src/veins_inet/HospitalControlApp.cc
@@ -217,7 +217,7 @@ void HospitalControlApp::onWSM(BaseFrame1609_4 *wsm){
         }
         if(!Constant::SHORTEST_PATH){
             double t = simTime().dbl();
-            if(t > 278.754313406148){
+            if(t > 440.3){
                 EV_TRACE<<t<<endl;
             }
             std::string newRoute = readMessage(bc);
