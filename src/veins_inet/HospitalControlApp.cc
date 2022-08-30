962b9a7f552077778026b3dcaa0a5fac8e5e842b
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-08-30 15:42:22 +0700

    usage of DecisionDijkstra has begun

--- src/veins_inet/HospitalControlApp.cc
+++ src/veins_inet/HospitalControlApp.cc
@@ -37,7 +37,8 @@ void HospitalControlApp::initialize(int stage)
     if (stage == 0) {
         sendBeacon= new cMessage("send Beacon");
         graph = new Graph();
-        djisktra = new HarmfulnessDijkstra();
+        djisktra = new DecisionDijkstra();
+                //new HarmfulnessDijkstra();
                     //new ArrivalDijkstra();
                    //new Djisktra();
         this->readCrossing();
