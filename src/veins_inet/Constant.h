47b4bbade2602ed848327e878e285a1a80e06380
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-09-08 06:18:59 +0700

    expected max latency for each AGV

--- src/veins_inet/Constant.h
+++ src/veins_inet/Constant.h
@@ -55,6 +55,7 @@ public:
     static constexpr const bool STOP_AT_STATION = true;
     static constexpr const int PAUSING_TIME = 10;
     static std::map<std::string, std::string> routeDict;
+    static constexpr const int THRESHOLD = 5;
 };
 
 static char* mergeContent(long Id){
