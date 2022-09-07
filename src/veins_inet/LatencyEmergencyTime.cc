1b65e3264298e1a00123aa55413dd39acf11f23c
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-09-08 00:33:51 +0700

    implement of new algorithm: compare latency time and then emergency one

--- src/veins_inet/LatencyEmergencyTime.cc
+++ src/veins_inet/LatencyEmergencyTime.cc
@@ -34,17 +34,19 @@ double LatencyEmergencyTime::getHarmfulnessArrival(AGV* cur, double time){
         return 0;
     double result = 0; //allSs[cur->itinerary->station]->getHarmfulness(time, cur->indexOfRoute, this->s, this->l);
     int count = cur->indexOfRoute;
-    if(cur->bestTime + count*cur->period - cur->amplitude <= time &&
-            cur->bestTime + count*cur->period + cur->amplitude >= time
+    double bestTime = this->allSs[cur->itinerary->station]->bestTime;
+    double period = this->allSs[cur->itinerary->station]->period;
+    double amplitude = this->allSs[cur->itinerary->station]->amplitude;
+    if(bestTime + count*period - amplitude <= time && bestTime + count*period + amplitude >= time
     ){
         return 0;
     }
-    if(cur->bestTime + count*cur->period - cur->amplitude > time){
-        result = cur->bestTime + count*cur->period - cur->amplitude - time;
+    if(bestTime + count*period - amplitude > time){
+        result = bestTime + count*period - amplitude - time;
         result /= 2;
     }
     else{
-        result = time - (cur->bestTime + count*cur->period + cur->amplitude);
+        result = time - (bestTime + count*period + amplitude);
     }
     return result;
 }
@@ -53,106 +55,139 @@ void LatencyEmergencyTime::DijkstrasAlgorithm(//std::vector <Quad> adjList[],
         int source, int target, std::string currLane, AGV* cur){
   Quad info; //(-1, "", -1, "");
   std::string trace;
-  double weight, objective;
-  double tempW;
-  int tempIndex;
-  double timeForEmergencyMode = 0;
   double now = cur->now;
-  std::string tempTime = "";
-  if(currLane.compare(cur->itinerary->laneId) != 0){
-      now = cur->ShortestPath[source];
+  this->cur = cur;
+  this->currLane = currLane;
+  if(this->currLane.compare(this->cur->itinerary->laneId) != 0){
+      now = this->cur->ShortestPath[source];
   }
-  double ratio = cur->ratio;
+  double ratio = this->cur->ratio;
   std::string tempTrace;
-  int index = findI_Vertex(currLane, false);
-  double firstCost = firstValue(currLane, vertices[index]);
+  int index = findI_Vertex(this->currLane, false);
+  double firstCost = firstValue(this->currLane, vertices[index]);
 
-  cur->init(numVertices);
-  cur->ShortestPath[source] = this->getHarmfulnessArrival(cur, ratio * firstCost + now);
+  this->cur->init(numVertices);
+  this->cur->ShortestPath[source] = this->getHarmfulnessArrival(this->cur, ratio * firstCost + now);
 
-  int x = cur->PQ.size();
+  this->cur->PQ1.push(std::make_tuple(this->cur->ShortestPath[source], 0, source, " 0")); // Source has weight cur->ShortestPath[source];
 
-  cur->PQ.push(std::make_tuple(cur->ShortestPath[source], 0, source, " 0")); // Source has weight cur->ShortestPath[source];
-
-  while (!cur->PQ.empty()){
-    x = cur->PQ.size();
-    info = cur->PQ.top(); // Use to get minimum weight
-    cur->PQ.pop(); // Pop before checking for cycles
-    x = cur->PQ.size();
-    cur->count = cur->count + 1;
+  while (!this->cur->PQ1.empty()){
+    info = this->cur->PQ1.top(); // Use to get minimum weight
+    this->cur->PQ1.pop(); // Pop before checking for cycles
+    this->cur->count = cur->count + 1;
     source = std::get<2>(info); // get the vertex
     if(source == target){
-        while (!cur->PQ.empty())
-            cur->PQ.pop();
+        std::string last = std::get<3>(info);
+        extractTraceAndTime(&last, NULL);
+        if(cur->traces[target].compare(last) != 0){
+            cur->traces[target] = last;
+        }
+        while (!this->cur->PQ1.empty())
+            this->cur->PQ1.pop();
         break;
     }
-    objective = std::get<0>(info); // current distance
-    weight = std::get<1>(info);
-    trace = std::get<3>(info);
-    tempTime = "";
-    for(int i = trace.length() - 1; i > 0; i--){
-        if((trace.at(i) >= '0' && trace.at(i) <= '9') || trace.at(i) == '.'){
-            tempTime = trace.at(i) + tempTime;
-        }
-        else{
-            break;
-        }
-    }
 
 
-
-    if (cur->visitedVertex.at(source)) // Check for cycle
-      continue; // Already accounted for it, move on
-
-    cur->visitedVertex.at(source) = true; // Else, mark the vertex so that we won't have to visit it again
-
-    for (std::vector<Quad>::iterator it = adjList[source].begin(); it != adjList[source].end(); it++){
-      tempW = /*(*it).weight; */std::get<0>(*it);
-      //tempW = std::get<1>(*it);
-      tempTrace = /*(*it).trace; */std::get<3>(*it);
-      tempIndex = /*(*it).source; */ std::get<2>(*it);
-      if(!Constant::SHORTEST_PATH){
-          timeWeightVertices[tempIndex] = this->expSmoothing->getDampingValue(tempIndex, timeWeightVertices[tempIndex], vertices[tempIndex]);
-      }
-
-      double newWeight = 0, newObjective = 0; //weight + tempW + 40*weightVertices[tempIndex];
-
-      if(!isValidTrace(currLane, tempTrace)){
-          continue;
-      }
-      if(isAntidromic(trace, tempTrace)){
-          continue;
-      }
-      newWeight = weight + tempW;
-      if(!Constant::SHORTEST_PATH){
-          double weightSmoothing = timeWeightVertices[tempIndex];
-          if(weightSmoothing < 0.1 && tempIndex < this->numIVertices){
-              newWeight += 100*(this->expSmoothing->useCycicalData(newWeight, vertices[tempIndex], weightSmoothing));
-          }
-          else{
-              newWeight += 100*weightSmoothing;
-          }
-      }
-      newWeight += firstCost;
-      //newObjective = (ratio * (newWeight) + now);
-      newObjective = this->getHarmfulnessArrival(cur, ratio * (newWeight) + now);
-      newObjective += objective/*tempW;*/ /*now*/;
-      if (newObjective < cur->ShortestPath[tempIndex]){ // Check if we can do better
-         cur->ShortestPath[tempIndex] = newObjective; // Update new distance
-         cur->traces[tempIndex] = trace; //tempTrace;
-         if(tempIndex == 26){
-             EV<<"DEEDDWF"<<endl;
-         }
-         if(tempTrace.length() == 0 && trace.length() == 0)
-         {
-             EV<<"ENULLLL"<<endl;
-         }
-
-         //std::string content = vertices[tempIndex] + "_" + cur->id;
-         std::string newTrace = trace + tempTrace;
-         cur->PQ.push(make_tuple(newObjective, newWeight, /*content,*/ tempIndex, newTrace));
-         x = cur->PQ.size();
-      } // Update distance
-    }
+    bool activeEdges = true;
+    this->checkActiveEdges(firstCost, &info, activeEdges);
+    this->checkActiveEdges(firstCost, &info, false);
   } // While Priority Queue is not empty
 } // DijkstrasAlgorithm
+
+void LatencyEmergencyTime::checkActiveEdges(double firstCost, Quad* info, bool activeEdges){
+    std::string trace;
+    double weight = 0, objective = 0;
+    double tempW;
+    int tempIndex;
+    double timeForEmergencyMode = 0;
+
+    double now = cur->now;
+    double ratio = cur->ratio;
+    std::string tempTrace;
+    objective = std::get<0>(*info); // current distance
+    weight = std::get<1>(*info);
+    int source = std::get<2>(*info);
+    if(!activeEdges){
+        if(source >= this->numIVertices)
+            source = std::get<2>(this->adjList[source][0]);
+    }
+    if(activeEdges){
+        if (this->cur->visitedVertex.at(source)) // Check for cycle
+            return; // Already accounted for it, move on
+    }
+    else{
+        if (this->cur->visitedEmergencyVertex.at(source)) // Check for cycle
+             return; // Already accounted for it, move on
+    }
+
+    if(activeEdges){
+        this->cur->visitedVertex.at(source) = true; // Else, mark the vertex so that we won't have to visit it again
+    }
+    else{
+        this->cur->visitedEmergencyVertex.at(source) = true;
+    }
+
+    trace = std::get<3>(*info);
+    extractTraceAndTime(&trace, &timeForEmergencyMode);
+    std::vector<std::vector<Quad>> *list = activeEdges ? &adjList : &emergencyAdjList;
+    for (std::vector<Quad>::iterator it = (*list)[source].begin(); it != (*list)[source].end(); it++){
+        tempW = /*(*it).weight; */std::get<0>(*it);
+        tempTrace = /*(*it).trace; */std::get<3>(*it);
+        double tempEmergencyTime = 0;
+        extractTraceAndTime(&tempTrace, &tempEmergencyTime);
+        tempIndex = /*(*it).source; */ std::get<2>(*it);
+        if(!Constant::SHORTEST_PATH){
+            if(activeEdges){
+                timeWeightVertices[tempIndex] = this->expSmoothing->getDampingValue(tempIndex, timeWeightVertices[tempIndex], vertices[tempIndex]);
+            }
+            else{
+                timeW_E_Vertices[tempIndex] = this->expSmoothing->getDampingValue(tempIndex, timeW_E_Vertices[tempIndex], vertices[tempIndex]);
+            }
+        }
+
+        double newWeight = 0, newObjective = 0; //weight + tempW + 40*weightVertices[tempIndex];
+
+        if(!isValidTrace(currLane, tempTrace)){
+            continue;
+        }
+        if(isAntidromic(trace, tempTrace)){
+            continue;
+        }
+        newWeight = weight + tempW;
+        if(!Constant::SHORTEST_PATH){
+            double weightSmoothing = activeEdges ? timeWeightVertices[tempIndex] : timeW_E_Vertices[tempIndex];
+            if(weightSmoothing < 0.1 && tempIndex < this->numIVertices && activeEdges){
+                newWeight += 100*(this->expSmoothing->useCycicalData(newWeight, vertices[tempIndex], weightSmoothing));
+            }
+            else{
+                newWeight += 100*weightSmoothing;
+            }
+        }
+
+        if(!activeEdges){
+            //newObjective += this->getHarmfulnessEmergency(ratio * tempW);//tempW la trong so cua canh khan cap
+        }
+
+        //newObjective += this->getHarmfulnessAvailable(ratio * tempW, tempTrace);
+
+        newWeight += firstCost;
+
+        newObjective += this->getHarmfulnessArrival(cur, ratio * (newWeight) + now);
+
+        newObjective += objective/*tempW;*/ /*now*/;
+        if (newObjective < cur->ShortestPath[tempIndex]){ // Check if we can do better
+            cur->ShortestPath[tempIndex] = newObjective; // Update new distance
+            cur->traces[tempIndex] = trace; //tempTrace;
+            if(activeEdges){
+                tempW = timeForEmergencyMode;
+            }
+            else{
+                tempW = ratio*tempW + timeForEmergencyMode;
+            }
+            //std::string content = vertices[tempIndex] + "_" + cur->id;
+            std::string newTrace = trace + tempTrace + " " + std::to_string(tempW);
+            cur->PQ1.push(make_tuple(newObjective, newWeight, /*content,*/ tempIndex, newTrace));
+        } // Update distance
+    }//End of for
+}
+
