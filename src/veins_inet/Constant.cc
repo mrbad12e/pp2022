6610928d7edb1c04b0986d924473207a9f46dbda
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-11-11 08:03:29 +0700

    new static variable

--- src/veins_inet/Constant.cc
+++ src/veins_inet/Constant.cc
@@ -24,6 +24,7 @@ double Constant::GLOBAL_HARMFULNESS = 0;
 double Constant::GLOBAL_SONNER = 0;
 double Constant::GLOBAL_LATER = 0;
 double Constant::EMERGENCY_TIME = 0;
+int Constant::edgeIdCnt = 0;
 std::map<std::string, std::string> Constant::routeDict = {{"route_0", ""}};
 //std::string Constant::SUPPLY_DISPOSAL = "$";
 Constant::Constant() {
